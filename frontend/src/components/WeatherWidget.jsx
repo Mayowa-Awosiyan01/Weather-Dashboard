81314cdec065e91fdcd48bd3c99812cdb0c58e4e
Mayowa Awosiyan <61680329+Mayowa-Awosiyan01@users.noreply.github.com>
2025-06-27 02:58:35 -0400

    Bootstrap Added

---
+++ frontend/src/components/WeatherWidget.jsx
@@ -0,0 +1,11 @@
+export default function WeatherWidget(){
+    return(
+        <div className="Container">
+            <div>
+                <h3>Toronto, Ontario</h3>
+                <div></div>
+            </div>
+        </div>
+    )
+
+}
